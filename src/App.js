0c100ebf37d1d360d5f58ad1db02457070618ae4
akhil <akhil.rajarao@xelpmoc.in>
2023-03-30 16:46:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import MobileDetails from "./components/MobileDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>...</h1>
+        <MobileDetails />
+      </header>
+    </div>
+  );
+}
+
+export default App;
